beecb8fc6b5c902777660e6642b18623b11242ea
amilah0883 <nafmz0883@gmail.com>
2025-11-18 23:58:37 +0000

    spreads weirdly. need to fix

--- CAPyle_releaseV2/release/ca_descriptions/templates/template2d.py
+++ CAPyle_releaseV2/release/ca_descriptions/templates/template2d.py
@@ -1,4 +1,4 @@
-# Name: NAME
+# Name: Modelling Forest Fire
 # Dimensions: 2
 
 # --- Set up executable path, do not edit ---
@@ -14,7 +14,7 @@ sys.path.append(main_dir_loc + 'capyle/guicomponents')
 
 from capyle.ca import Grid2D, Neighbourhood, randomise2d
 import capyle.utils as utils
-
+import numpy as np
 
 def setup(args):
     """Set up the config object used to interact with the GUI"""
@@ -48,6 +48,13 @@ def setup(args):
     # config.grid_dims = (200,200)
 
     # ----------------------------------------------------------------------
+    config.fill_in_defaults()
+    POWER_STATION = "LEFT" # "RIGHT"
+
+    if POWER_STATION == "LEFT":
+        config.initial_grid[0,0] = 5
+    elif POWER_STATION == "RIGHT":
+        config.initial_grid[0,-1] = 5
 
     # the GUI calls this to pass the user defined config
     # into the main system with an extra argument
@@ -57,13 +64,63 @@ def setup(args):
         sys.exit()
     return config
 
-
 def transition_function(grid, neighbourstates, neighbourcounts):
     """Function to apply the transition rules
     and return the new grid"""
     # YOUR CODE HERE
-    return grid
 
+    # STATES
+    NO_ELEMENT=0 
+    FOREST = 1
+    LAKE = 2
+    CHAPARRAL = 3
+    CANYON = 4
+    BURNING = 5
+    BURNT = 6
+
+    #Ignite probability
+    IGNITE_PROB = {
+        NO_ELEMENT: 0.9,
+        FOREST: 0.1,
+        LAKE: 0.0,
+        CHAPARRAL: 0.5,
+        CANYON: 0.7
+    }
+
+    #Burn Duration
+    # 1 iteration = 1/2 day
+    BURN_DURATION = {
+        NO_ELEMENT:30,
+        FOREST: 60,        #30 days
+        LAKE: 0,
+        CHAPARRAL: 14,     #7 days
+        CANYON: 1          #1/2 day
+    }
+    
+    burning_neighbours = neighbourcounts[BURNING]
+
+    #new grid bcz some cells change state, some don't
+    current = grid.copy()
+    new_grid = grid.copy()
+
+    for terrain in (NO_ELEMENT, FOREST, LAKE, CHAPARRAL, CANYON):
+
+        prob = IGNITE_PROB[terrain]
+        if prob == 0:
+            continue #skip lake
+
+        # find cells that will burn
+        adjacency = (current == terrain) & (burning_neighbours>0)
+
+        # method to decide to burn
+        rand = np.random.random(grid.shape)
+        ignite = adjacency & (rand <prob)
+
+        # ignite
+        new_grid[ignite] = BURNING
+        new_grid [grid == BURNING] = BURNING
+
+    return new_grid
 
 def main():
     """ Main function that sets up, runs and saves CA"""
