e2c73231cf6dbb517660bd98afacbfe36eb57468
William Levine <23029518+william-levine@users.noreply.github.com>
2025-11-25 23:46:06 +0000

    Added random wind direction option in the gui

--- CAPyle_releaseV2/release/ca_descriptions/templates/template2d.py
+++ CAPyle_releaseV2/release/ca_descriptions/templates/template2d.py
@@ -109,6 +109,8 @@ def transition_function(grid, neighbourstates, neighbourcounts, decay_grid, conf
     # 0:NW, 1:N, 2:NE, 3:W, 4:E, 5:SW, 6:S, 7:SE = neighbourstates
     wind_direction = config.wind_direction
 
+    print(f"Direction: {wind_direction}")
+
     # neighbourcounts stores the number of neighbour for each state
     chaparral, forest, lake, canyon, town, burning, burnt = neighbourcounts
     notburning = (chaparral, forest, lake,canyon)
--- CAPyle_releaseV2/release/capyle/guicomponents/windtoggleui.py
+++ CAPyle_releaseV2/release/capyle/guicomponents/windtoggleui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from capyle.guicomponents import _ConfigUIComponent
+import numpy as np
 
 class _WindToggleUI(tk.Frame, _ConfigUIComponent):
 
@@ -17,7 +18,7 @@ class _WindToggleUI(tk.Frame, _ConfigUIComponent):
         
         self.wind_direction = tk.StringVar()
 
-        self.options = 'North-West', 'North', 'North-East', 'West', 'East', 'South-West', 'South', 'South-East'
+        self.options = 'North-West', 'North', 'North-East', 'West', 'East', 'South-West', 'South', 'South-East', 'Random'
         self.optvar = tk.StringVar(self)
         self.option = self.options[0]
         self.optvar.set(self.options[0])
@@ -29,7 +30,11 @@ class _WindToggleUI(tk.Frame, _ConfigUIComponent):
     def get_value(self):
         # return self.options_map[self.optvar.get()]
         choice = self.optvar.get()
-        return self.options.index(choice)
+
+        if choice == "Random":
+            return np.random.randint(0,8)
+        else:
+            return self.options.index(choice)
 
     def set(self, value):
         self.wind_direction.set(value)
