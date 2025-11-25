8332cf8ab63c990e1b15f82cb1ce27e2c8aeb34b
amilah0883 <nafmz0883@gmail.com>
2025-11-25 01:47:24 +0000

    fire only start on the left even change to right

---
+++ CAPyle_releaseV2/release/capyle/guicomponents/firetoggleui.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from capyle.guicomponents import _ConfigUIComponent
+
+class _FireToggleUI(tk.Frame, _ConfigUIComponent):
+
+    def __init__(self, parent, ca_config):
+        tk.Frame.__init__(self, parent)
+        _ConfigUIComponent.__init__(self)
+
+        self.ca_config = ca_config
+
+        # upper frame to hold title and dropdown box
+        labelframe = tk.Frame(self)
+        gen_label = tk.Label(labelframe, text='Fire Start:')
+        gen_label.pack(side=tk.LEFT)
+        
+        self.start_fire = tk.StringVar()
+
+        self.options = 'LEFT', 'RIGHT'
+        self.optvar = tk.StringVar(self)
+        self.optvar.set(self.options[0])
+        self.optbox = tk.OptionMenu(labelframe, self.optvar, *self.options)
+        self.optbox.config(width=9)
+        self.optbox.pack(side=tk.LEFT)
+        labelframe.pack()
+
+    def get_value(self):
+        return self.start_fire.get()
+
+    def set(self, value):
+        self.start_fire.set(value)
+
+    def set_default(self):
+        self.start_fire.set("LEFT")
